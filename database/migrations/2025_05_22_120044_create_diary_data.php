163e112045683399a49aeb1958364492cfeb7c67
Sheyla <sheyla.rahman1436@gmail.com>
2025-05-24 09:00:54 +0700

    Update 2025_05_22_120044_create_diary_data.php

--- database/migrations/2025_05_22_120044_create_diary_data.php
+++ database/migrations/2025_05_22_120044_create_diary_data.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('diary')->nullable();
             $table->date('date_created')->default(DB::raw('CURRENT_DATE'));
-            $table->smallInteger('mood_rate');
+            $table->enum('mood', ['awful', 'bad', 'so-so', 'good', 'terrific']);
             $table->binary('photo')->nullable();
             $table->timestamps();
             
