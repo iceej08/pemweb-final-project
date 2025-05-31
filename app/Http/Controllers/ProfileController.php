00cdb2680be99f9e6160eceee2dccbb7491b9ed7
Sheyla <99881992+terserahdeh@users.noreply.github.com>
2025-05-31 08:16:40 +0700

    add profile controller

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class ProfileController extends Controller
+{
+    public function index($username)
+    {
+        $user = User::where('username', $username)->firstOrFail();
+        return view('profile', compact('user'));
+    }
+
+    public function update(Request $request, $username, $id)
+    {
+        $user = User::findOrFail($id);     
+
+        $request->validate([
+            'new_name' => 'required|string',
+            'new_username' => 'required|string|unique:user_moodiary,username,'. $user->id,
+            'new_email' => 'required|string|unique:user_moodiary,email,'. $user->id,
+        ]);
+
+        $user->name = $request->new_name;
+        $user->username = $request->new_username;
+        $user->email = $request->new_email;
+
+        $user->save();
+        return redirect()->route('profile', ['username' => $user->username])->with('success', 'Profil berhasil diperbarui!');
+    }
+}
